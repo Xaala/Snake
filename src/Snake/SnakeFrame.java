01d3d88547e593b60cc175b8b60133478381a8c0
Xaala <ditchendorfc93@gmail.com>
2020-07-26 15:02:55 -0400

    Updated external container to JFrame, centered game window, added boarder.

--- src/Snake/SnakeFrame.java
+++ src/Snake/SnakeFrame.java
@@ -1,26 +1,26 @@
 package Snake;
 
+import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
-import java.awt.event.*;
 
-public class SnakeFrame extends Frame
+public class SnakeFrame extends JFrame
 {
+
     public SnakeFrame()
     {
         System.out.printf("Making Snake Frame.");
         SnakeGame game = new SnakeGame();
 
-        //Handle window close signal so we can actually shutdown on 'X' click.
-        addWindowListener(new WindowAdapter()
-        {
-            public void windowClosing(WindowEvent e)
-            {
-                dispose();
-            }
-        });
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        this.setSize(450,450);
         this.add(game, BorderLayout.CENTER);
+
+        this.setLayout(new CardLayout(10,10));
+
+        this.setSize(450,475);
+        this.setLocationRelativeTo(null);
+        getRootPane().setBorder(BorderFactory.createMatteBorder(4, 4, 4, 4, Color.BLACK));
         this.setVisible(true);
 
         //Initialize game and begin running.
