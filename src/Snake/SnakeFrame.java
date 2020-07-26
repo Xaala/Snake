936406466b0a8fdde633f088c1ad63783e377d2e
Xaala <ditchendorfc93@gmail.com>
2020-07-26 13:39:52 -0400

    Added ability to actually handle application shutdown correctly.

--- src/Snake/SnakeFrame.java
+++ src/Snake/SnakeFrame.java
@@ -3,52 +3,33 @@ package Snake;
 import java.awt.*;
 import java.awt.event.*;
 
-public class SnakeFrame extends Frame implements WindowListener
+public class SnakeFrame extends Frame
 {
     public SnakeFrame()
     {
         System.out.printf("Making Snake Frame.");
-        addWindowListener(this);
         SnakeGame game = new SnakeGame();
-        Frame myFrame = new Frame("Snake Game");
 
-        myFrame.setSize(450,450);
+        //Handle window close signal so we can actually shutdown on 'X' click.
+        addWindowListener(new WindowAdapter()
+        {
+            public void windowClosing(WindowEvent e)
+            {
+                dispose();
+            }
+        });
 
-        myFrame.add(game, BorderLayout.CENTER);
 
-        myFrame.setVisible(true);
+        this.setSize(450,450);
+        this.add(game, BorderLayout.CENTER);
+        this.setVisible(true);
 
+        //Initialize game and begin running.
         game.init();
-
     }
 
     public static void main(String[] args)
     {
         new SnakeFrame();
     }
-
-    @Override
-    public void windowOpened(WindowEvent e) { }
-
-    @Override
-    public void windowClosing(WindowEvent e)
-    {
-        System.out.println("closing");
-        dispose();
-    }
-
-    @Override
-    public void windowClosed(WindowEvent e) { }
-
-    @Override
-    public void windowIconified(WindowEvent e) { }
-
-    @Override
-    public void windowDeiconified(WindowEvent e) { }
-
-    @Override
-    public void windowActivated(WindowEvent e) { }
-
-    @Override
-    public void windowDeactivated(WindowEvent e) { }
 }
